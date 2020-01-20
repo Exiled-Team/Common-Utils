e7321bf0224ef635c974accbe71dd5e2690c552e
Kade <KadePcGames@gmail.com>
2020-01-20 14:50:36 -0800

    1.2b

--- Common-Utils/KConf/ExiledConfiguration.cs
+++ Common-Utils/KConf/ExiledConfiguration.cs
@@ -17,11 +17,34 @@ namespace Common_Utils.KConf
         public static Dictionary<string,string> GetDictonaryValue(string value)
         {
             Dictionary<string, string> dict = new Dictionary<string, string>();
+            if (!value.Contains(","))
+            {
+                dict.Add(value.Split(':')[0], value.Split(':')[1]);
+                return dict;
+            }
             string[] tl = value.Split(',');
             foreach(string t in tl)
             {
                 string[] vl = t.Split(':');
                 dict.Add(vl[0],vl[1]);
+            }
+            return dict;
+        }
+
+        public static List<string> GetListStringValue(string value)
+        {
+            if (value == null)
+                return null;
+            List<string> dict = new List<string>();
+            if (!value.Contains(","))
+            {
+                dict.Add(value);
+                return dict;
+            }
+            string[] tl = value.Split(',');
+            foreach (string t in tl)
+            {
+                dict.Add(t);
             }
             return dict;
         }
