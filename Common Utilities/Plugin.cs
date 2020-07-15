90684f9ecee04be294c558a2fcd14a259eeaea0c
galaxy119 <amathor929@gmail.com>
2020-07-15 14:53:08 -0700

    Version bump

--- Common Utilities/Plugin.cs
+++ Common Utilities/Plugin.cs
@@ -17,8 +17,8 @@ namespace Common_Utilities
     {
         public override string Name { get; } = "Common Utilities";
         public override string Author { get; } = "Galaxy119";
-        public override Version Version { get; } = new Version(1, 0, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 1);
         public override string Prefix { get; } = "CommonUtilities";
         
         public PlayerHandlers PlayerHandlers;
