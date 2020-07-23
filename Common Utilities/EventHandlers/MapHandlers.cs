48acbd0b339cb7c09029a76fb18b783a1a7a1c77
xRoier <58123692+xRoier@users.noreply.github.com>
2020-07-23 18:45:37 +0200

    Fix 914CustomRecipes & Add support for upgrading items in-hand

--- Common Utilities/EventHandlers/MapHandlers.cs
+++ Common Utilities/EventHandlers/MapHandlers.cs
@@ -12,17 +12,33 @@ namespace Common_Utilities.EventHandlers
         public void OnScp914UpgradingItems(UpgradingItemsEventArgs ev)
         {
             if (plugin.Config.Scp914Configs.ContainsKey(ev.KnobSetting))
+            {
                 foreach (Pickup item in ev.Items)
                 {
                     foreach ((ItemType sourceItem, ItemType destinationItem, int chance) in plugin.Config.Scp914Configs[ev.KnobSetting])
                     {
-                        if (sourceItem != item.ItemId) 
-                            continue;
-                        
-                        if (plugin.Gen.Next(100) <= chance)
-                            UpgradeItem(item, destinationItem);
+                        if (sourceItem == item.ItemId)
+                        {
+                            if (plugin.Gen.Next(100) <= chance)
+                                UpgradeItem(item, destinationItem);
+                        }
                     }
                 }
+                foreach (Player pla in ev.Players)
+                {
+                    foreach ((ItemType sourceItem, ItemType destinationItem, int chance) in plugin.Config.Scp914Configs[ev.KnobSetting])
+                    {
+                        if (pla.CurrentItem.id == sourceItem)
+                        {
+                            if (plugin.Gen.Next(100) <= chance)
+                            {
+                                pla.ReferenceHub.inventory.NetworkcurItem = destinationItem;
+                            }
+                        }
+                    }
+                }
+
+            }
         }
 
         internal void UpgradeItem(Pickup oldItem, ItemType newItem)
