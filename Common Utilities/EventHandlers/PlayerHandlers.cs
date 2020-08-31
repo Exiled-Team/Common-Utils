c8aa693b33a99d7af4573b895a93f3e8bcbd285f
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-08-31 13:24:29 +0200

    Update PlayerHandlers.cs

--- Common Utilities/EventHandlers/PlayerHandlers.cs
+++ Common Utilities/EventHandlers/PlayerHandlers.cs
@@ -2,12 +2,13 @@ namespace Common_Utilities.EventHandlers
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Linq;
 	using Exiled.API.Features;
 	using Exiled.Events.EventArgs;
 	using MEC;
 	using Respawning;
+	using UnityEngine;
 	using Player = Exiled.API.Features.Player;
-
 	public class PlayerHandlers
 	{
 		private readonly Plugin plugin;
@@ -19,7 +20,6 @@ namespace Common_Utilities.EventHandlers
 			if (!string.IsNullOrEmpty(message))
 				ev.Player.Broadcast(plugin.Config.JoinMessageDuration, message);
 		}
-
 		public void OnChangingRole(ChangingRoleEventArgs ev)
 		{
 			if (plugin.Config.Inventories.ContainsKey(ev.NewRole))
@@ -34,7 +34,7 @@ namespace Common_Utilities.EventHandlers
 					ev.Player.Health = plugin.Config.Health[ev.NewRole];
 					ev.Player.MaxHealth = plugin.Config.Health[ev.NewRole];
 				});
-			
+
 			if (plugin.Config.AnnounceDclassScientistsElimination)
 			{
 				if (ev.Player.Role == RoleType.ClassD && Player.Get(RoleType.ClassD).IsEmpty()) 
@@ -49,10 +49,8 @@ namespace Common_Utilities.EventHandlers
 		{
 			if (plugin.Config.HealOnKill.ContainsKey(ev.Killer.Role))
 			{
-				if (ev.Killer.Health + plugin.Config.HealOnKill[ev.Killer.Role] <= ev.Killer.MaxHealth)
-					ev.Killer.Health += plugin.Config.HealOnKill[ev.Killer.Role];
-				else
-					ev.Killer.Health = ev.Killer.MaxHealth;
+				if (ev.Killer.Health + plugin.Config.HealOnKill[ev.Killer.Role] <= ev.Killer.MaxHealth)  ev.Killer.Health += plugin.Config.HealOnKill[ev.Killer.Role];
+				else ev.Killer.Health = ev.Killer.MaxHealth;
 			}
 		}
 
@@ -60,10 +58,14 @@ namespace Common_Utilities.EventHandlers
 		{
 			if (ev.Target != ev.Attacker)
 			{
-				if (plugin.Config.ExtraAmnesia > 0 && ev.Attacker.Role.Is939()) ev.Target.ReferenceHub.playerEffectsController.EnableEffect<CustomPlayerEffects.Amnesia>(plugin.Config.ExtraAmnesia);
-				if (plugin.Config.Scp0492Damage != 40 && ev.Attacker.Role == RoleType.Scp0492) ev.Amount = plugin.Config.Scp0492Damage;
+				if (plugin.Config.Scp049Damage != 4949 && ev.Attacker.Role == RoleType.Scp049) ev.Amount = plugin.Config.Scp049Damage;
+				else if (plugin.Config.Scp0492Damage != 40 && ev.Attacker.Role == RoleType.Scp0492) ev.Amount = plugin.Config.Scp0492Damage;
+				else if (plugin.Config.Scp096Damage != 9696 && ev.Attacker.Role == RoleType.Scp096) ev.Amount = plugin.Config.Scp096Damage;
 				else if (plugin.Config.Scp106Damage != 40 && ev.Attacker.Role == RoleType.Scp106) ev.Amount = plugin.Config.Scp106Damage;
-				else if (plugin.Config.Scp939Damage != 65 && ev.Attacker.Role.Is939()) ev.Amount = plugin.Config.Scp939Damage;
+				else if (plugin.Config.Scp173Damage != 999990 && ev.Attacker.Role == RoleType.Scp173) ev.Amount = plugin.Config.Scp173Damage;
+				else if (ev.Attacker.Role.Is939())
+					if (plugin.Config.Scp939Damage != 65) ev.Amount = plugin.Config.Scp939Damage;
+					if (plugin.Config.ExtraAmnesia > 0) ev.Target.ReferenceHub.playerEffectsController.EnableEffect<CustomPlayerEffects.Amnesia>(plugin.Config.ExtraAmnesia);
 			}
 		}
 
