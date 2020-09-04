e4965c4971a8ac24574b1561809930631c98362a
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-04 03:25:44 +0200

    Update Plugin.cs

--- Common Utilities/Plugin.cs
+++ Common Utilities/Plugin.cs
@@ -2,96 +2,88 @@ using HarmonyLib;
 
 namespace Common_Utilities
 {
-    using System;
-    using System.Collections.Generic;
-    using Common_Utilities.EventHandlers;
-    using MEC;
-    using Exiled.API.Features;
-    using Server = Exiled.Events.Handlers.Server;
-    using Player = Exiled.Events.Handlers.Player;
-    using Scp914 = Exiled.Events.Handlers.Scp914;
+	using System;
+	using System.Collections.Generic;
+	using Common_Utilities.EventHandlers;
+	using MEC;
+	using Exiled.API.Features;
+	using Server = Exiled.Events.Handlers.Server;
+	using Player = Exiled.Events.Handlers.Player;
+	using Scp914 = Exiled.Events.Handlers.Scp914;
 
-    public class Plugin : Exiled.API.Features.Plugin<Config>
-    {
-        public override string Name { get; } = "Common Utilities";
-        public override string Author { get; } = "Galaxy119";
-        public override Version Version { get; } = new Version(1, 0, 8);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
-        public override string Prefix { get; } = "CommonUtilities";
-        
-        public PlayerHandlers PlayerHandlers;
-        public ServerHandlers ServerHandlers;
-        public MapHandlers MapHandlers;
-        public List<CoroutineHandle> Coroutines = new List<CoroutineHandle>();
-        public Random Gen = new Random();
-        public static Plugin Singleton;
-        public Harmony Instance;
+	public class Plugin : Exiled.API.Features.Plugin<Config>
+	{
+		public override string Name { get; } = "Common Utilities";
+		public override string Author { get; } = "Galaxy119";
+		public override Version Version { get; } = new Version(1, 0, 8);
+		public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
+		public override string Prefix { get; } = "CommonUtilities";
+		
+		public Handlers Handlers;
+		public List<CoroutineHandle> Coroutines = new List<CoroutineHandle>();
+		public Random Gen = new Random();
+		public static Plugin Singleton;
+		public Harmony Instance;
 
-        public override void OnEnabled()
-        {
-            Singleton = this;
-            
-            Log.Info($"Parsing config..");
-            try
-            {
-                Log.Debug($"Parsing inventory config..", Config.Debug);
-                Config.ParseInventorySettings();
-                Log.Debug($"Parsing health config..", Config.Debug);
-                Config.ParseHealthSettings();
-                Log.Debug($"Parsing health on kill config..", Config.Debug);
-                Config.ParseHealthOnKill();
-                Log.Debug($"Parsing 914 config..", Config.Debug);
-                Config.Parse914Settings();
-                Log.Debug($"Parsing 914 role config..", Config.Debug);
-                Config.Parse914ClassChanges();
-            }
-            catch (Exception e)
-            {
-                Log.Error($"Ya fucked up the parsing.");
-                Log.Error($"{e.Message}\n{e.StackTrace}");
-            }
-
-            Log.Info($"Instantiating Events..");
-            PlayerHandlers = new PlayerHandlers(this);
-            ServerHandlers = new ServerHandlers(this);
-            MapHandlers = new MapHandlers(this);
-            
-            Log.Info($"Registering EventHandlers..");
-            Player.Joined += PlayerHandlers.OnPlayerJoined;
-            Player.ChangingRole += PlayerHandlers.OnChangingRole;
-            Player.Died += PlayerHandlers.OnPlayerDied;
-			Player.Hurting += PlayerHandlers.OnPlayerHurt;
+		public override void OnEnabled()
+		{
+			Singleton = this;
 			
-			Server.SendingConsoleCommand += ServerHandlers.OnConsoleCommand;
-            Server.RoundStarted += ServerHandlers.OnRoundStarted;
-            Server.WaitingForPlayers += ServerHandlers.OnWaitingForPlayers;
-            Server.RoundEnded += ServerHandlers.OnRoundEnded;
-            Server.RestartingRound += ServerHandlers.OnRestartingRound;
+			Log.Info($"Parsing config..");
+			try
+			{
+				Log.Debug($"Parsing inventory config..", Config.Debug);
+				Config.ParseInventorySettings();
+				Log.Debug($"Parsing health config..", Config.Debug);
+				Config.ParseHealthSettings();
+				Log.Debug($"Parsing health on kill config..", Config.Debug);
+				Config.ParseHealthOnKill();
+				Log.Debug($"Parsing 914 config..", Config.Debug);
+				Config.Parse914Settings();
+				Log.Debug($"Parsing 914 role config..", Config.Debug);
+				Config.Parse914ClassChanges();
+			}
+			catch (Exception e)
+			{
+				Log.Error($"Ya fucked up the parsing.");
+				Log.Error($"{e.Message}\n{e.StackTrace}");
+			}
+
+			Log.Info($"Instantiating Events..");
+			Handlers = new Handlers(this);
 			
-            Scp914.UpgradingItems += MapHandlers.OnScp914UpgradingItems;
+			Log.Info($"Registering EventHandlers..");
+			Scp914.UpgradingItems += Handlers.OnScp914UpgradingItems;
+			Player.Joined += Handlers.OnPlayerJoined;
+			Player.ChangingRole += Handlers.OnChangingRole;
+			Player.Died += Handlers.OnPlayerDied;
+			Player.Hurting += Handlers.OnPlayerHurt;
+			Server.SendingConsoleCommand += Handlers.OnConsoleCommand;
+			Server.RoundStarted += Handlers.OnRoundStarted;
+			Server.WaitingForPlayers += Handlers.OnWaitingForPlayers;
+			Server.RoundEnded += Handlers.OnRoundEnded;
+			Server.RestartingRound += Handlers.OnRestartingRound;
 
-            Instance = new Harmony("com.galaxy.cu");
-            Instance.PatchAll();
+			Instance = new Harmony("com.galaxy.cu");
+			Instance.PatchAll();
 
-            base.OnEnabled();
-        }
+			base.OnEnabled();
+		}
 
-        public override void OnDisabled()
-        {
-            Player.Joined -= PlayerHandlers.OnPlayerJoined;
-            Player.ChangingRole -= PlayerHandlers.OnChangingRole;
-            Player.Died -= PlayerHandlers.OnPlayerDied;
-			Player.Hurting-= PlayerHandlers.OnPlayerHurt;
-			
-            Server.RoundStarted -= ServerHandlers.OnRoundStarted;
-            Server.WaitingForPlayers -= ServerHandlers.OnWaitingForPlayers;
-            Server.RoundEnded -= ServerHandlers.OnRoundEnded;
-			Server.SendingConsoleCommand -= ServerHandlers.OnConsoleCommand;
-			
-            Scp914.UpgradingItems -= MapHandlers.OnScp914UpgradingItems;
-            Instance.UnpatchAll();
+		public override void OnDisabled()
+		{
+			Scp914.UpgradingItems -= Handlers.OnScp914UpgradingItems;
+			Player.Joined -= Handlers.OnPlayerJoined;
+			Player.ChangingRole -= Handlers.OnChangingRole;
+			Player.Died -= Handlers.OnPlayerDied;
+			Player.Hurting-= Handlers.OnPlayerHurt;
+			Server.RoundStarted -= Handlers.OnRoundStarted;
+			Server.WaitingForPlayers -= Handlers.OnWaitingForPlayers;
+			Server.RoundEnded -= Handlers.OnRoundEnded;
+			Server.SendingConsoleCommand -= Handlers.OnConsoleCommand;
+			Instance.UnpatchAll();
 
-            base.OnDisabled();
-        }
-    }
+			base.OnDisabled();
+		}
+	}
 }
