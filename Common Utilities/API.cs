cc925bbc4c4ba650b20feb831bdddf839aa0557b
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-04 03:26:03 +0200

    Update API.cs

--- Common Utilities/API.cs
+++ Common Utilities/API.cs
@@ -1,10 +1,10 @@
 namespace Common_Utilities
 {
     using System.Collections.Generic;
-    
+
     public static class API
     {
-        public static List<ItemType> GetStartItems(RoleType role) => Plugin.Singleton.PlayerHandlers.StartItems(role);
+        public static List<ItemType> GetStartItems(RoleType role) => Plugin.Singleton.Handlers.StartItems(role);
 
         public static float GetHealthOnKill(RoleType role) => Plugin.Singleton.Config.HealOnKill.ContainsKey(role) ? Plugin.Singleton.Config.HealOnKill[role] : 0f;
     }
