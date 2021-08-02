8099499e6a112adf92911c13cb183c2090698350
RedRanger26 <61168189+slid88@users.noreply.github.com>
2021-08-01 22:06:53 -0500

    Update Scp049SpeechPatch.cs

--- Common Utilities/Patches/Scp049SpeechPatch.cs
+++ Common Utilities/Patches/Scp049SpeechPatch.cs
@@ -1,22 +1,40 @@
 using Assets._Scripts.Dissonance;
 using HarmonyLib;
 
-namespace Common_Utilities.Patches
+namespace VoiceChatSCP
 {
     [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.CallCmdAltIsActive))]
-    public class Scp049Speak
+    public static class SpeechPatch
     {
         public static bool Prefix(DissonanceUserSetup __instance, bool value)
         {
-            if (!Plugin.Singleton.Config.Scp049Speech)
-                return true;
-			
             CharacterClassManager ccm = __instance.gameObject.GetComponent<CharacterClassManager>();
 
-            if (ccm.CurClass == RoleType.Scp049 || ccm.CurClass.Is939()) 
-                __instance.MimicAs939 = value;
+            if (!HandleConfigLogic(ccm.CurClass)) return true;
+
+            __instance.MimicAs939 = value;
 
             return true;
+        }
+
+        private static bool HandleConfigLogic(RoleType role)
+        {
+            switch(role)
+            {
+                case RoleType.Scp096:
+                    return Plugin.Singleton.Config.CanShyGuySpeak;
+                case RoleType.Scp173:
+                    return Plugin.Singleton.Config.CanPeanutSpeak;
+                case RoleType.Scp106:
+                    return Plugin.Singleton.Config.CanLarrySpeak;
+                case RoleType.Scp0492:
+                    return Plugin.Singleton.Config.CanZombieSpeak;
+                case RoleType.Scp049:
+                    return Plugin.Singleton.Config.CanDoctorSpeak;
+                default:
+                    return true;
+            }
+
         }
     }
 }
