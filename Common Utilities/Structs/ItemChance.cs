30ccd54bcf83601c5e8e1c2864072ff431c9e3de
PintTheDragon <PintTheDragon@hotmail.com>
2021-08-22 21:02:12 -0700

    Just call it group

--- Common Utilities/Structs/ItemChance.cs
+++ Common Utilities/Structs/ItemChance.cs
@@ -4,13 +4,13 @@ namespace Common_Utilities.Structs
     {
         public string ItemName { get; set; }
         public int Chance { get; set; }
-        public string GroupKey { get; set; }
+        public string Group { get; set; }
 
         public void Deconstruct(out string name, out int i, out string groupKey)
         {
             name = ItemName;
             i = Chance;
-            groupKey = GroupKey;
+            groupKey = Group;
         }
     }
 }
