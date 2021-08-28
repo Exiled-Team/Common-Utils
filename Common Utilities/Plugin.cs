bb11a0962a76f91ab1806482ac10169254719a28
Joker <amathor929@gmail.com>
2021-08-27 20:50:39 -0700

    recompile for alpha.71

--- Common Utilities/Plugin.cs
+++ Common Utilities/Plugin.cs
@@ -18,7 +18,7 @@ namespace Common_Utilities
     {
         public override string Name { get; } = "Common Utilities";
         public override string Author { get; } = "Galaxy119";
-        public override Version Version { get; } = new Version(4, 0, 2);
+        public override Version Version { get; } = new Version(4, 0, 3);
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
         public override string Prefix { get; } = "CommonUtilities";
         
