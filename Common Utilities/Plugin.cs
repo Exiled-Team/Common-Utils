cc32885216311d8542175fc853baaa166a5ef612
Joker <amathor929@gmail.com>
2021-09-02 17:07:44 -0700

    version bump

--- Common Utilities/Plugin.cs
+++ Common Utilities/Plugin.cs
@@ -21,7 +21,6 @@ namespace Common_Utilities
     {
         public override string Name { get; } = "Common Utilities";
         public override string Author { get; } = "Galaxy119";
-        public override Version Version { get; } = new Version(4, 1, 0);
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
         public override string Prefix { get; } = "CommonUtilities";
         
