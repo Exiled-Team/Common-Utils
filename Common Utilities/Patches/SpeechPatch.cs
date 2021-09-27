2e63452fa849f6247fb1fc598cf66e3266d827ee
sanyae2439 <30918724+sanyae2439@users.noreply.github.com>
2021-09-27 13:31:32 +0900

    Fix tabs

--- Common Utilities/Patches/SpeechPatch.cs
+++ Common Utilities/Patches/SpeechPatch.cs
@@ -2,7 +2,7 @@ using HarmonyLib;
 
 namespace Common_Utilities.Patches
 {
-	[HarmonyPatch(typeof(Radio), nameof(Radio.UserCode_CmdSyncTransmissionStatus))]
+    [HarmonyPatch(typeof(Radio), nameof(Radio.UserCode_CmdSyncTransmissionStatus))]
     public static class SpeechPatch
     {
         public static bool Prefix(Radio __instance, bool b)
